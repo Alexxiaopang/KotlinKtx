e884c7342f92b245e8839820949ae7f711b658d0
Alex <610>
2021-03-18 15:13:52 +0800

    加入协程

--- app/src/main/java/com/android/comandroidktxxx/MainActivity.kt
+++ app/src/main/java/com/android/comandroidktxxx/MainActivity.kt
@@ -2,6 +2,7 @@ package com.android.comandroidktxxx
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.android.ktx.spanner.Spanner
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ kt_ktx/src/main/java/com/android/ktx/coroutinescope/CoroutineScopeExt.kt
@@ -0,0 +1,29 @@
+package com.android.ktx.coroutinescope
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.async
+
+
+suspend inline fun <T> CoroutineScope.newAsync(crossinline action: suspend () -> T): Deferred<T> {
+    return async(SupervisorJob()) {
+        action.invoke()
+    }
+}
+
+suspend fun <T> Deferred<T>.tryAwait(): T? {
+    return try {
+        await()
+    } catch (e: Throwable) {
+        null
+    }
+}
+
+suspend inline fun <T> Deferred<T>.tryAwaitReturn(crossinline action: Throwable.() -> T): T {
+    return try {
+        await()
+    } catch (e: Throwable) {
+        action.invoke(e)
+    }
+}
